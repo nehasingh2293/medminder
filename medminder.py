c2743fb57e88af5ff395a718b2196fa413c67298
Alykhan Kanji <github@alykhan.com>
2014-09-13 01:33:46 -0400

    Add arrow and time helper func

--- medminder.py
+++ medminder.py
@@ -4,18 +4,21 @@
 
 import os
 
+import arrow
 from flask import Flask
 import redis
 
 app = Flask(__name__)
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+def get_current_time():
+    return arrow.now()
+
 @app.route('/')
 def root():
-    return 'Hello World!'
+    return 'Current time: ' + get_current_time().format('dddd HH:mm')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     PORT = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=PORT)
-    
