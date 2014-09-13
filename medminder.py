de35c6c2a385d05fe3b02b73f93ae282dc0041e7
Neha Singh <neha.singh2293@gmail.com>
2014-09-13 01:22:30 -0400

    Configure redis

--- medminder.py
+++ medminder.py
@@ -5,8 +5,10 @@
 import os
 
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @app.route('/')
 def root():
@@ -16,3 +18,4 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     PORT = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=PORT)
+    
